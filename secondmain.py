c44dc1c7d45c6c54be8248a5c048d18cb4a01d2e
Felipe Cook <felipepcook@gmail.com>
2020-05-14 14:35:56 -0600

    + testing manipulation of strings in Python

--- secondmain.py
+++ secondmain.py
@@ -1,9 +1,26 @@
 my_shopping_list = ['apples', 'bananas', 'eggplant']
 
 
+# iterates through the above list.
 def my_program():
     for i in my_shopping_list:
         print(i)
 
 
 my_program()
+
+test_text = "hello world"
+
+
+# this slices the above test from the sixth character onward.
+def my_second_program():
+    print(test_text[6:])
+
+
+# this grabs a portion of the above text from the 2nd position (inclusive) to the 5th position exclusive
+def my_third_program():
+    print(test_text[2:5])
+
+
+my_second_program()
+my_third_program()
